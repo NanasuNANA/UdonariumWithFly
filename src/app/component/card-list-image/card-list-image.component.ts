a2a4cf7fd19b443f7eb26f94aeb4e85dbfd8c921
DESKTOP-64JNFCF\keyra <nnanasu7@gmail.com>
2023-04-08 00:47:15 +0900

    アニメーション追加。

--- src/app/component/card-list-image/card-list-image.component.ts
+++ src/app/component/card-list-image/card-list-image.component.ts
@@ -1,3 +1,4 @@
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Card, CardState } from '@udonarium/card';
 import { StringUtil } from '@udonarium/core/system/util/string-util';
@@ -5,7 +6,34 @@ import { StringUtil } from '@udonarium/core/system/util/string-util';
 @Component({
   selector: 'card-list-image',
   templateUrl: './card-list-image.component.html',
-  styleUrls: ['./card-list-image.component.css']
+  styleUrls: ['./card-list-image.component.css'],
+  animations: [
+    trigger('reverseCard', [
+      transition(':increment', [
+        animate('132ms ease-in-out', keyframes([
+          style({ transform: 'rotateZ(0deg)', offset: 0 }),
+          style({ transform: 'rotateZ(90deg)', offset: 0.5 }),
+          style({ transform: 'rotateZ(180deg)', offset: 1.0 })
+        ]))
+      ]),
+      transition(':decrement', [
+        animate('132ms ease-in-out', keyframes([
+          style({ transform: 'rotateZ(180deg)', offset: 0 }),
+          style({ transform: 'rotateZ(270deg)', offset: 0.5 }),
+          style({ transform: 'rotateZ(360deg)', offset: 1.0 })
+        ]))
+      ])
+    ]),
+    trigger('flipCard', [
+      transition(':increment,:decrement', [
+        animate('132ms ease-in-out', keyframes([
+          style({ transform: 'scaleX(1.0)', offset: 0 }),
+          style({ transform: 'scaleX(0.1)', offset: 0.5 }),
+          style({ transform: 'scaleX(1.0)', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class CardListImageComponent implements OnInit {
   @Input() card: Card = null;
