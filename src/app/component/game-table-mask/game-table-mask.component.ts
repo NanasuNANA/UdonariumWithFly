c9dfc3e7a85ee7bb0f394f7ec5ae7330ab906dd2
DESKTOP-64JNFCF\keyra <nnanasu7@gmail.com>
2023-12-24 11:40:52 +0900

    文言変更。

--- src/app/component/game-table-mask/game-table-mask.component.ts
+++ src/app/component/game-table-mask/game-table-mask.component.ts
@@ -680,11 +680,11 @@ export class GameTableMaskComponent implements OnChanges, OnDestroy, AfterViewIn
       },
       ContextMenuSeparator,
       {
-        name: 'ボーダーの表示',
+        name: 'ボーダーラインの表示',
         subActions: [
           { name: `${this.borderType == 0 ? '◉' : '○'} 操作時のみ`,  action: () => { this.borderType = 0 }, checkBox: 'radio' },
           { name: `${this.borderType == 1 ? '◉' : '○'} 操作、非固定時`,  action: () => { this.borderType = 1 }, checkBox: 'radio' },
-          { name: `${this.borderType == 2 ? '◉' : '○'} 常に表示`,  action: () => { this.borderType = 2 }, checkBox: 'radio' },
+          { name: `${this.borderType == 2 ? '◉' : '○'} 固定時も表示`,  action: () => { this.borderType = 2 }, checkBox: 'radio' },
         ],
         disabled: this.isScratching
       },
